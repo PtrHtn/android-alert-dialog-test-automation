5e979564279596afd7bf11ab818f2295a6971f06
PtrHtn <ptrhtn@gmail.com>
2021-02-19 20:10:06 -0300

    Create HomeScreenViewModel.kt

---
+++ app/src/main/java/com/ptrhtn/androidalertdialoguitestautomation/HomeScreenViewModel.kt
@@ -0,0 +1,7 @@
+package com.ptrhtn.androidalertdialoguitestautomation
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class HomeScreenViewModel(application: Application) : AndroidViewModel(application) {
+}
