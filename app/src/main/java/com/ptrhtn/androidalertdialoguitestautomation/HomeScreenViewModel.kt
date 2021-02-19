f4a9e03807a8b4267ce8ed4bce2e55792843ab37
PtrHtn <ptrhtn@gmail.com>
2021-02-19 20:13:28 -0300

    Add sharedPreferencesService variable to HomeScreenViewModel.kt

--- app/src/main/java/com/ptrhtn/androidalertdialoguitestautomation/HomeScreenViewModel.kt
+++ app/src/main/java/com/ptrhtn/androidalertdialoguitestautomation/HomeScreenViewModel.kt
@@ -4,4 +4,6 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 
 class HomeScreenViewModel(application: Application) : AndroidViewModel(application) {
+    private val sharedPreferencesService = SharedPreferencesService(application)
+
 }
