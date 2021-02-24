024b282562c34f3f595b86a680031513beb979d1
PtrHtn <ptrhtn@gmail.com>
2021-02-24 20:47:08 -0300

    Update HomeScreenFragment.kt

--- app/src/main/java/com/ptrhtn/androidalertdialoguitestautomation/HomeScreenFragment.kt
+++ app/src/main/java/com/ptrhtn/androidalertdialoguitestautomation/HomeScreenFragment.kt
@@ -10,7 +10,6 @@ class HomeScreenFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home_screen, container, false)
     }
 
