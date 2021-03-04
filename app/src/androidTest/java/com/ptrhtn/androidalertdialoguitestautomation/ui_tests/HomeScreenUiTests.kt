fe19462d0db4248c64247ba0bac1669bc3feaae4
PtrHtn <ptrhtn@gmail.com>
2021-03-04 00:05:31 -0300

    Add shouldDisplayAllHomeScreenViews_asExpected test into HomeScreenUiTests.kt

---
+++ app/src/androidTest/java/com/ptrhtn/androidalertdialoguitestautomation/ui_tests/HomeScreenUiTests.kt
@@ -0,0 +1,27 @@
+package com.ptrhtn.androidalertdialoguitestautomation.ui_tests
+
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
+import com.ptrhtn.androidalertdialoguitestautomation.MainActivity
+import com.ptrhtn.androidalertdialoguitestautomation.screen_objects.HomeScreen
+import org.junit.Rule
+import org.junit.Test
+
+@LargeTest
+class HomeScreenUiTests : TestCase() {
+    @get:Rule
+    val activityTestRule = ActivityTestRule(MainActivity::class.java, true, false)
+
+    @Test
+    fun shouldDisplayAllHomeScreenViews_asExpected() {
+        before {
+            activityTestRule.launchActivity(null)
+        }.after {
+        }.run {
+            step("Open Home screen and check title") {
+                HomeScreen { homeScreenTitle.isDisplayed() }
+            }
+        }
+    }
+}
