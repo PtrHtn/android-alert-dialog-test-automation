92788e39155ff0b882c16a48b17b1bfc49b60033
PtrHtn <ptrhtn@gmail.com>
2021-03-04 00:04:51 -0300

    Add screen object homeScreenTitle into HomeScreen.kt

--- app/src/androidTest/java/com/ptrhtn/androidalertdialoguitestautomation/screen_objects/HomeScreen.kt
+++ app/src/androidTest/java/com/ptrhtn/androidalertdialoguitestautomation/screen_objects/HomeScreen.kt
@@ -1,4 +1,17 @@
 package com.ptrhtn.androidalertdialoguitestautomation.screen_objects
 
-object HomeScreen {
+import com.agoda.kakao.common.views.KView
+import com.agoda.kakao.text.KTextView
+import com.kaspersky.kaspresso.screens.KScreen
+import com.ptrhtn.androidalertdialoguitestautomation.HomeScreenFragment
+import com.ptrhtn.androidalertdialoguitestautomation.R
+
+object HomeScreen : KScreen<HomeScreen>() {
+    override val layoutId: Int?
+        get() = R.layout.fragment_home_screen
+    override val viewClass: Class<*>?
+        get() = HomeScreenFragment::class.java
+
+    val homeScreenTitle = KView { withText("Android Alert Dialogs")}
+
 }
