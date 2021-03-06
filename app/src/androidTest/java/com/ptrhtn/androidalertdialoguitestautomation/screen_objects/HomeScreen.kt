c6b458b1fac4cf616f704a6754a9fbf42a66c6a5
PtrHtn <ptrhtn@gmail.com>
2021-03-06 13:47:39 -0300

    Add radio buttons screen ojects to the HomeScreen.kt

--- app/src/androidTest/java/com/ptrhtn/androidalertdialoguitestautomation/screen_objects/HomeScreen.kt
+++ app/src/androidTest/java/com/ptrhtn/androidalertdialoguitestautomation/screen_objects/HomeScreen.kt
@@ -14,4 +14,10 @@ object HomeScreen : KScreen<HomeScreen>() {
 
     val homeScreenTitle = KView { withText("Android Alert Dialogs")}
 
+    val radioButtonsLayout = KView { withId(R.id.home_screen_radio_buttons_layout)}
+    val radioButtonsTitle = KTextView{ withId(R.id.home_screen_radio_buttons_title_textView)}
+    val radioButtonsDescription = KTextView { withId(R.id.home_screen_radio_buttons_description_textView)}
+    val textRadioButton = KTextView{withId(R.id.home_screen_radio_button_text_textView)}
+    val radioButtonsValue = KTextView { withId(R.id.home_screen_radio_buttons_value_textView)}
+
 }
