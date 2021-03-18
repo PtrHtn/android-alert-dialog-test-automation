83cac229cf6940ca5b9b4bea639f80ed5951d6dd
PtrHtn <ptrhtn@gmail.com>
2021-03-18 00:07:10 -0300

    Add variable homeScreenViewModel into HomeScreenFragment.kt

--- app/src/main/java/com/ptrhtn/androidalertdialoguitestautomation/HomeScreenFragment.kt
+++ app/src/main/java/com/ptrhtn/androidalertdialoguitestautomation/HomeScreenFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 
 class HomeScreenFragment : Fragment() {
+    private lateinit var homeScreenViewModel: HomeScreenViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
