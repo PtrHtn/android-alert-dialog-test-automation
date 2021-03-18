fbe22526ffcb13be79c2e542b9f14440f7cb8249
PtrHtn <ptrhtn@gmail.com>
2021-03-17 23:59:28 -0300

    Add radioWave variable  as a MutableLiveData HomeScreenViewModel.kt

--- app/src/main/java/com/ptrhtn/androidalertdialoguitestautomation/HomeScreenViewModel.kt
+++ app/src/main/java/com/ptrhtn/androidalertdialoguitestautomation/HomeScreenViewModel.kt
@@ -2,8 +2,11 @@ package com.ptrhtn.androidalertdialoguitestautomation
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
 
 class HomeScreenViewModel(application: Application) : AndroidViewModel(application) {
     private val sharedPreferencesService = SharedPreferencesService(application)
+
+    val radioWave by lazy { MutableLiveData<String>() }
 
 }
