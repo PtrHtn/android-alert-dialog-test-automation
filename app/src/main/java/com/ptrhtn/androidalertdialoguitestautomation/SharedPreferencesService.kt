85e42dc637de6e42b3f71076ff8290189e2ad362
PtrHtn <ptrhtn@gmail.com>
2021-03-18 00:04:50 -0300

    Add getRadioWave method to the SharedPreferencesService.kt

--- app/src/main/java/com/ptrhtn/androidalertdialoguitestautomation/SharedPreferencesService.kt
+++ app/src/main/java/com/ptrhtn/androidalertdialoguitestautomation/SharedPreferencesService.kt
@@ -3,4 +3,14 @@ package com.ptrhtn.androidalertdialoguitestautomation
 import android.content.Context
 
 class SharedPreferencesService(context: Context) {
+
+    private val sharedPreferences =
+            context.getSharedPreferences("AndroidAlertDialog_SharedPreferences", Context.MODE_PRIVATE)
+
+    fun getRadioWave(): String? {
+        return when (sharedPreferences.getString("radioWave", "FM").isNullOrBlank()) {
+            true -> "FM"
+            false -> sharedPreferences.getString("radioWave", "FM")
+        }
+    }
 }
