074363726eeed0fe3a392b7b84981f3bc088c0fe
PtrHtn <ptrhtn@gmail.com>
2021-03-27 21:51:01 -0300

    Add radioWaveValueObserver in HomeScreenFragment.kt

--- app/src/main/java/com/ptrhtn/androidalertdialoguitestautomation/HomeScreenFragment.kt
+++ app/src/main/java/com/ptrhtn/androidalertdialoguitestautomation/HomeScreenFragment.kt
@@ -5,9 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+
+import android.app.Activity
+import android.Manifest
+import android.widget.TextView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.Observer
+
 
 class HomeScreenFragment : Fragment() {
     private lateinit var homeScreenViewModel: HomeScreenViewModel
+
+    private val radioWaveValueObserver = Observer<String>{radioWaveValue ->
+        radioWaveValue?.let {
+            view?.findViewById<TextView>(R.id.home_screen_radio_buttons_value_textView)
+        }
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
