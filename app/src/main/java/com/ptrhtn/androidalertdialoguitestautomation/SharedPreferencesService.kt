6bf64c8ba97ef41797bfbcdf47c804fe2f57fd08
PtrHtn <ptrhtn@gmail.com>
2021-06-27 22:11:26 -0300

    Add method save to the SharedPreferencesService

--- app/src/main/java/com/ptrhtn/androidalertdialoguitestautomation/SharedPreferencesService.kt
+++ app/src/main/java/com/ptrhtn/androidalertdialoguitestautomation/SharedPreferencesService.kt
@@ -1,16 +1,27 @@
 package com.ptrhtn.androidalertdialoguitestautomation
 
 import android.content.Context
+import android.content.SharedPreferences
 
 class SharedPreferencesService(context: Context) {
 
     private val sharedPreferences =
             context.getSharedPreferences("AndroidAlertDialog_SharedPreferences", Context.MODE_PRIVATE)
 
-    fun getRadioWave(): String? {
-        return when (sharedPreferences.getString("radioWave", "FM").isNullOrBlank()) {
+    fun getRadioButtonChoice(): String? {
+        return when (sharedPreferences.getString("radioButtonChoice", "FM").isNullOrBlank()) {
             true -> "FM"
-            false -> sharedPreferences.getString("radioWave", "FM")
+            false -> sharedPreferences.getString("radioButtonChoice", "FM")
+        }
+    }
+
+    fun save(key: String, value: String) {
+
+        val editor: SharedPreferences.Editor = sharedPreferences.edit()
+
+        with(editor) {
+            putString(key, value)
+            apply()
         }
     }
 }
